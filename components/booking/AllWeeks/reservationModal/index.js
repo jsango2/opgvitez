62cd9f8ee4d6ec5eeffdc2b4980ff3fdab08bfc1
jsango2 <jure.sango@gmail.com>
2022-06-04 13:17:27 +0200

    form submitting reservation

--- components/booking/AllWeeks/reservationModal/index.js
+++ components/booking/AllWeeks/reservationModal/index.js
@@ -13,30 +13,36 @@ const encode = (data) => {
     .join("&");
 };
 
-function ReservationModal({ handleClick, price, datum }) {
-  const [data, setData] = useState([]);
-  const [formData, setFormData] = useState([]);
-  const [selectedWeeks, setSelectedWeeks] = useState([]);
-  const [suma, setSuma] = useState(0);
-  const [cijena, setCijena] = useState(0);
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState();
-  const [free, setFree] = useState("Free");
-  const [selected, setSelected] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-  const [logedIn, setlogedIn] = useState(null);
-
-  const handleSubmit = (e) => {
+class ReservationModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      cijena: 0,
+      message: "",
+      free: "Free",
+      selected: false,
+    };
+  }
+  // const [data, setData] = useState([]);
+  // const [formData, setFormData] = useState([]);
+  // const [selectedWeeks, setSelectedWeeks] = useState([]);
+  // const [suma, setSuma] = useState(0);
+  // const [cijena, setCijena] = useState(0);
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState();
+  // const [free, setFree] = useState("Free");
+  // const [selected, setSelected] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [logedIn, setlogedIn] = useState(null);
+  handleSubmit = (e) => {
     // dataLayer.push({ event: "PRO form bottom submitted" })
-    setFormData({
-      emaiL: email,
-      message: message,
-      booking: datum,
-    });
+    console.log(this.state);
+
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "reservation", formData }),
+      body: encode({ "form-name": "contact", ...this.state }),
     }).then((res) => {
       // this.setState({ showModal: true });
       // window.dataLayer.push({
@@ -64,67 +70,62 @@ function ReservationModal({ handleClick, price, datum }) {
 
     e.preventDefault();
   };
-  function insert(str, value) {
+  insert = (str, value) => {
     let position = str.length - 3;
     return str.substr(0, position) + value + str.substr(position);
+  };
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  render() {
+    const { message, email, cijena, free, selected } = this.state;
+    return (
+      <WrapSection>
+        <CloseX onClick={this.props.handleClick}>X</CloseX>
+        <Fixed>
+          Dates selected: <br />{" "}
+        </Fixed>
+        <WrapDates>
+          {this.props.datum.map((el) => (
+            <Week>
+              <Date>{el.datum}</Date>
+              <Price>{this.insert(el.cijena.toString(), ".")} EUR</Price>
+            </Week>
+          ))}
+        </WrapDates>
+        <Fixed>Total price: {this.props.price} EUR</Fixed>
+        <form
+          name="contact-form"
+          netlify
+          netlify-honeypot="bot-field"
+          style={{ display: "none" }}
+        >
+          <input type="hidden" name="form-name" value="contact-form" />
+          <input type="text" name="message" />
+          <input type="email" name="email" />
+        </form>
+        <form onSubmit={this.handleSubmit}>
+          {/* <Fixed>Email:</Fixed> */}
+          <input
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={email}
+            required
+            onChange={this.handleChange}
+          />
+          {/* <Fixed>Message:</Fixed> */}
+
+          <textarea
+            placeholder="Your message"
+            name="message"
+            rows="4"
+            value={message}
+            onChange={this.handleChange}
+          ></textarea>
+          <button type="submit"> Send reservation enquire</button>
+        </form>
+      </WrapSection>
+    );
   }
-  return (
-    <WrapSection>
-      <form
-        name="reservation"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        hidden
-      >
-        <input type="hidden" name="form-name" value="reservation" />
-        <input type="text" name="ime" />
-        <input type="email" name="email" />
-      </form>
-
-      <CloseX onClick={handleClick}>X</CloseX>
-      <Fixed>
-        Dates selected: <br />{" "}
-      </Fixed>
-      <WrapDates>
-        {datum.map((el) => (
-          <Week>
-            <Date>{el.datum}</Date>
-            <Price>{insert(el.cijena.toString(), ".")} EUR</Price>
-          </Week>
-        ))}
-      </WrapDates>
-      <Fixed>Total price: {price} EUR</Fixed>
-
-      <form
-        onSubmit={handleSubmit}
-        name="reservation"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="reservation" />
-        {/* <Fixed>Email:</Fixed> */}
-        <input
-          placeholder="Email"
-          type="email"
-          name="email"
-          value={email}
-          required
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        {/* <Fixed>Message:</Fixed> */}
-
-        <textarea
-          placeholder="Your message"
-          name="message"
-          rows="4"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-        ></textarea>
-        <button type="submit"> Send reservation enquire</button>
-      </form>
-    </WrapSection>
-  );
 }
 
 export default ReservationModal;
